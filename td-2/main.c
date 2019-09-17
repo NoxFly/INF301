c8542680d8377a198e49ed3d1f41eff164c7325a
NoxFly <dorian.thivolle@gmail.com>
2019-09-17 13:39:59 +0200

    works on VS does not work on putty

--- td-2/main.c
+++ td-2/main.c
@@ -3,62 +3,97 @@
 
 #define MAX_LEN 1000
 
-void encode(char *uncoded, char *encoded);
-int indexOf(char *str, char c);
-void splice(char *str, int idx);
+void encode(char uncoded[MAX_LEN], char encoded[MAX_LEN]);
+void decode(char encoded[MAX_LEN], char uncoded[MAX_LEN]);
+int indexOf(char* str, char c);
+void splice(char* str, int idx);
 
-int main(int argc, char **argv) {
-    char str[] = "abcbcca";
-    char encoded[MAX_LEN];
+int main(int argc, char** argv) {
+	char str[] = "abcbcca";
+	char str2[] = "abcaabc";
+	char encoded[MAX_LEN];
+	char uncoded[MAX_LEN];
 
-    encode(str, encoded);
+	encode(str, encoded);
+	decode(str2, uncoded);
 
-    return 0;
+	printf("encoded: %s\nuncoded: %s\n", encoded, uncoded);
+
+	return 0;
 }
 
-void encode(char *uncoded, char *encoded) {
-    char seq[26];
-    const int l = strlen(uncoded);
+void encode(char uncoded[MAX_LEN], char encoded[MAX_LEN]) {
+	const int l = strlen(uncoded);
+	char seq[26];
+	int j = 0;
 
-    for(int i=0; i<l; i++) {
+	for (int i = 0; i < l; i++) {
+		char c = uncoded[i], c2;
+		int idx;
 
-       char c = uncoded[i], c2;
-       int idx;
-
-       if((idx = indexOf(seq, c)) < -1) { // recupere l'index de c dans seq
-            // dans la sequence
-            if(idx > 0) c2 = seq[idx-1];
-            else c2 = seq[strlen(seq)-1];
-
-
-            encoded[i] = c2;
-            splice(seq, idx);
-
-       } else {
-           // pas dans la sequence
-            encoded[i] = c; // ajout du caractere dans le message code
-       }
-        
-        seq[strlen(seq)] = c; // ajout du caractere dans la seq
-    }
+		if ((idx = indexOf(seq, c)) > -1) { // recupere l'index de c dans seq
+			// dans la sequence
+			if (idx > 0) c2 = seq[idx - 1];
+			else c2 = seq[j];
 
 
+			splice(seq, idx);
+		}
+		else {
+			// pas dans la sequence
+			if(i>0) j++;
+			c2 = c;
+		}
+
+		encoded[i] = c2; // ajout du caractere dans le message code
+
+		seq[j] = c; // ajout du caractere dans la seq
+
+		printf("c: %c\n  idx: %i\n  seq: %s\n  c2: %c \n  encoded: %s\n  (j): %i\n\n", c, idx, seq, c2, encoded, j);
+	}
+}
+
+void decode(char encoded[MAX_LEN], char uncoded[MAX_LEN]) {
+	const int l = strlen(encoded);
+	char seq[26];
+	int j = 0;
+
+	for (int i = 0; i < l; i++) {
+		char c = encoded[i], c2;
+		int idx;
+
+		if ((idx = indexOf(seq, c)) > -1) { // recupere l'index de c dans seq
+			// dans la sequence
+			if (idx < j) c2 = seq[idx + 1];
+			else c2 = seq[0];
+
+			splice(seq, indexOf(seq, c2));
+		}
+		else {
+			// pas dans la sequence
+			if(i>0) j++;
+			c2 = c; // ajout du caractere dans le message code
+		}
+
+		uncoded[i] = c2;
+		seq[j] = c2; // ajout du caractere dans la seq
+	}
 }
 
 // retourne la position de c dans str. -1 si pas present
-int indexOf(char *str, char c) {
-    for(int i=0; i<strlen(str); i++) {
-        if(str[i] == c) return i;
-    }
+int indexOf(char* str, char c) {
+	for (int i = 0; i < strlen(str); i++) {
+		if (str[i] == c) return i;
+	}
 
-    return -1;
+	return -1;
 }
 
-void splice(char *str, int idx) {
-    int l = strlen(str);
-    int d = 0;
-    for(int i=0; i<l; i++) {
-        if(i == idx) d++;
-        else str[i-d] = str[i];
-    }
+void splice(char* str, int idx) {
+	int l = strlen(str);
+	int d = 0;
+	for (int i = 0; i < l; i++) {
+		if (i == idx) d++;
+		else str[i - d] = str[i];
+	}
 }
